21bf3c0f94e1c4cea99fef0428b81297c35ef020
frazynondo <Eugene@905>
2019-11-26 06:43:36 -0500

    first

---
+++ fntool/src/main/java/com/FrazyNondo/fntool/FntoolApplication.java
@@ -0,0 +1,13 @@
+package com.FrazyNondo.fntool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FntoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FntoolApplication.class, args);
+	}
+
+}
